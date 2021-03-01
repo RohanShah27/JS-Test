13bdd4a687c2683ff00d0eb5b4056b3c7ab5a792
Rohan <rohanshah2711@gmail.com>
2021-03-01 19:56:46 +0530

    added deafult image in case player image is not found

--- src/components/Players/index.js
+++ src/components/Players/index.js
@@ -9,6 +9,7 @@ export default class Players extends Component {
   //initialie the state
   state = {
     playerList: [],
+    searchString: "",
   };
   /**
    *@description Calls the given API endpoint to get the player data and then format it to display
@@ -28,13 +29,17 @@ export default class Players extends Component {
       //assign values
       playerObject.PFName = playerList[i].PFName;
       playerObject.SkillDesc = playerList[i].SkillDesc;
-      playerObject.Value = "$" + playerList[i].Value;
+      playerObject.Value = playerList[i].Value;
       playerObject.Id = playerList[i].Id;
       playerObject.UpcomingMatch = {
-        teams:
-          playerList[i].UpComingMatchesList[0].CCode +
-          " vs " +
-          playerList[i].UpComingMatchesList[0].VsCCode,
+        teams: {
+          team1: playerList[i].UpComingMatchesList[0].CCode
+            ? playerList[i].UpComingMatchesList[0].CCode
+            : "NA",
+          team2: playerList[i].UpComingMatchesList[0].VsCCode
+            ? playerList[i].UpComingMatchesList[0].VsCCode
+            : "NA",
+        },
         time: playerList[i].UpComingMatchesList[0].MDate,
       };
       playerObject.TName = playerList[i].TName;
@@ -49,37 +54,84 @@ export default class Players extends Component {
    * Checking if an image exist in your image folder
    */
   loadImage = (variable) => {
-    var image = new Image();
-    var url_image = "../../assets/player-images/" + variable + ".jpg";
-    image.src = url_image;
-    if (image.width === 0) {
-      return <img src={require("../../assets/player-images/63706.jpg")} />;
-    } else {
-      return (
-        <img src={require(`../../assets/player-images/${variable}.jpg`)} />
-      );
+    let image;
+    try {
+      image = require(`../../assets/player-images/${variable}.jpg`);
+      return image.default;
+    } catch (e) {
+      image = require("../../assets/player-images/default.jpg");
+      return image.default;
     }
+  };
+  //save search string and display searc based players
+  onChange = (event) => {
+    this.setState({ ...this.state, searchString: event.target.value });
+    let playerListCopy = [...this.state.playerList];
+    let found = playerListCopy.filter((player) => {
+      return (
+        player.PFName.toLowerCase().indexOf(
+          event.target.value.toLowerCase()
+        ) !== -1 ||
+        player.TName.toLowerCase().indexOf(event.target.value.toLowerCase()) !==
+          -1
+      );
+    });
+    console.log("found: ", found);
   };
   render() {
     return (
       <div className="main-container">
+        <div className="player-list-header">
+          <div>
+            <h2>Players</h2>
+          </div>
+          <div className="search-box">
+            <input
+              type="text"
+              onChange={(event) => {
+                this.onChange(event);
+              }}
+            />
+          </div>
+        </div>
         <div className="player-grid">
           {/* map the players here */}
           {this.state.playerList
-            //sort the player list in ascending order
+            //sort the player list in ascending order of Value
             .sort((a, b) =>
-              a.PFName.toLowerCase() < b.PFName.toLowerCase() ? -1 : 1
+              parseFloat(a.Value) < parseFloat(b.Value) ? -1 : 1
             )
             .map((player, index) => (
               <div className="player-card" key={`player-card-${index}`}>
                 {/* player image container */}
                 <div className="player-image">
-                  <img src={playerImage} />
+                  {<img src={this.loadImage(player.Id)} />}
                 </div>
                 <div className="player-details">
                   <div className="name-skill">
                     <div>Name : {player.PFName}</div>
                     <div>Skill : {player.SkillDesc}</div>
+                    <div>Value : $ {player.Value} bn</div>
+                  </div>
+                </div>
+                <div className="upcoming-container">
+                  <div>
+                    <h3>Upcoming Match</h3>
+                    <div className="match">
+                      {player.UpcomingMatch.teams.team1 +
+                        " vs " +
+                        player.UpcomingMatch.teams.team2}
+                    </div>
+                  </div>
+                  <div className="date-time">
+                    <h3>Date</h3>
+                    {/* date of the match */}
+                    {player.UpcomingMatch.time
+                      ? new Date(player.UpcomingMatch.time).toLocaleString(
+                          //get current locale from browser
+                          navigator.language
+                        )
+                      : "NA"}
                   </div>
                 </div>
               </div>
